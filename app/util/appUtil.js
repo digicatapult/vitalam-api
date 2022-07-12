0d7aa643fa1d817059071b654607d609fbf68182
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-07-12 15:42:07 +0100

    Fix padding issue

--- app/util/appUtil.js
+++ app/util/appUtil.js
@@ -328,9 +328,12 @@ async function getFile(base64Hash) {
 }
 
 const utf8ToHex = (str, len) => {
-  const buffer = Buffer.alloc(len)
-  buffer.write(str)
-  return `0x${buffer.toString('hex')}`
+  const buffer = Buffer.from(str, 'utf8')
+  const bufferHex = buffer.toString('hex')
+  if (bufferHex.length > 2 * len) {
+    throw new Error(`${str} is too long. Max length: ${len} bytes`)
+  }
+  return `0x${bufferHex}`
 }
 
 const hexToUtf8 = (str) => {

25f51c6295321c0ddfd1e2532f05717ad9a37f2d
Jonathan Gray <jonmattgray@gmail.com>
2022-11-25 16:38:32 +0000

    add api.catch() comment

--- app/util/substrateApi.js
+++ app/util/substrateApi.js
@@ -8,7 +8,7 @@ const { API_HOST, API_PORT } = env
 const provider = new WsProvider(`ws://${API_HOST}:${API_PORT}`)
 const api = new ApiPromise({ provider })
 
-api.isReadyOrError.catch(() => {})
+api.isReadyOrError.catch(() => {}) // prevent unhandled promise rejection errors
 
 api.on('disconnected', () => {
   logger.warn(`Disconnected from substrate node at ${API_HOST}:${API_PORT}`)
